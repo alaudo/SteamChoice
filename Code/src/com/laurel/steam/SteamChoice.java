50360a9182cdea885c01779c65da02a5bd25e193
Alexander Galkin (BING USA) <algalkin@microsoft.com>
2017-03-14 23:07:37 -0700

    Initial frame for IntelliJ project

---
+++ Code/src/com/laurel/steam/SteamChoice.java
@@ -0,0 +1,11 @@
+package com.laurel.steam;
+
+/**
+ * Created by Alexander Galkin on 3/14/2017.
+ */
+public class SteamChoice {
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+}
---
+++ Code/src/com/laurel/steam/Student.java
@@ -0,0 +1,8 @@
+package com.laurel.steam;
+
+/**
+ * Created by alexg on 3/14/2017.
+ */
+public interface Student {
+
+}
